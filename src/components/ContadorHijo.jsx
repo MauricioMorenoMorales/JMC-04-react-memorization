ec6aa0997aed289132c2a6833b6bef6319d73765
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-08-18 09:38:42 -0500

    Empezando la clase 75

--- src/components/ContadorHijo.jsx
+++ src/components/ContadorHijo.jsx
@@ -1,9 +1,25 @@
-import React, { memo } from 'react';
+import React, { memo, useMemo } from 'react';
 
 //Cuando este elemento no recibe nada siempre se renderizara cuándo cambie el estado de el padre <input type="text"/>
 //Cuando este elemento reciba un dato de el padre ya no se renderizara cuándo haya cambio en <input:text/>
 //Pero cuando ejecute funciones provenientes de el padre se volvera a renderizar cada vez que cambie el input y el <input:txt/>
 const ContadorHijo = ({ contador, sumar, restar }) => {
+	// let superNumero = 0;
+
+	// for (let i = 0; i < 1000000000; i++) {
+	// 	superNumero++;
+	// }
+
+	const superNumero = useMemo(() => {
+		let numero = 0;
+
+		for (let i = 0; i < 1000000000; i++) {
+			numero++;
+		}
+
+		return numero;
+	}, []);
+
 	console.log('Hijo Contador se Renderiza');
 	return (
 		<div style={{ border: 'thin solid #000', margin: '1rem', padding: '1rem' }}>
@@ -13,6 +29,7 @@ const ContadorHijo = ({ contador, sumar, restar }) => {
 				<button onClick={sumar}>+</button>
 				<button onClick={restar}>-</button>
 			</nav>
+			<h3>{superNumero}</h3>
 		</div>
 	);
 };
